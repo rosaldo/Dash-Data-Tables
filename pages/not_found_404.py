d4ef0d96603401fe3146df7664c955201e54b1e1
Rosaldo <rosaldo@gmail.com>
2022-09-26 15:14:54 -0300

    fist commit; basic structure

---
+++ pages/not_found_404.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import dash
+from dash import dcc
+
+dash.register_page(__name__, title="")
+
+
+def layout():
+    return dcc.Location(id="404_logout_location", href="/", refresh=True)
